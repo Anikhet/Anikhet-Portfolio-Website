4beb2dae8fa0890a1a01f99594f2839c55706e20
Anikhet Mulky <am9559@g.rit.edu>
2024-03-03 23:48:48 -0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+gsap.to("#intro",{
+    top: "-100%",delay:1,duration:0.5,
+    ease: "expo.out"
+})
+
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
-    smooth: true
+    smooth: true,
+    lerp: 0.07
+
 });
